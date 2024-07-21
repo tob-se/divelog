b3517c53c5f06397d54b601bb5d0851bdbd7dc6c
tob-se <41297085+tob-se@users.noreply.github.com>
2024-07-21 15:40:14 +0200

    remove google dependency

--- types/place.ts
+++ types/place.ts
@@ -9,13 +9,3 @@ export const placeSchema = z
   .strict();
 
 export type Place = z.infer<typeof placeSchema>;
-
-export const toPlaceFromPrediction = (
-  prediction: google.maps.places.AutocompletePrediction,
-): Place => {
-  return {
-    id: prediction.place_id,
-    main_text: prediction.structured_formatting.main_text,
-    secondary_text: prediction.structured_formatting.secondary_text,
-  };
-};
